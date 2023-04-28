89c9d6cc26c2f944d53f3d5ed6b28502c09b05f3
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2023-04-28 10:43:01 +0800

    Delete armvirt_64 directory

--- armvirt_64/diy.sh
+++
@@ -1,8 +0,0 @@
-
-
-SHELL_FOLDER=$(dirname $(readlink -f "$0"))
-bash $SHELL_FOLDER/../common/kernel_5.15.sh
-
-sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += perl btrfs-progs luci-app-amlogic luci-app-adguardhome luci-app-bypass -luci-app-autoreboot -luci-app-attendedsysupgrade -luci-app-gpsysupgrade fdisk lsblk luci-app-dockerman luci-app-vlmcsd luci-app-openvpn-server luci-app-tencentddns docker-compose luci-theme-argon luci-app-passwall luci-mod-rpc/' target/linux/armvirt/Makefile
-
-
